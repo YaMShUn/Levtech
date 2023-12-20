20815f06994391a5bd9a6b2c6d928247992a9552
YaMShUn <shuukatu122579@gmail.com>
2023-12-20 17:40:14 +0900

    ブログ投稿の練習

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ use App\Http\Controllers\PostController;      //追加
 |
 */
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
